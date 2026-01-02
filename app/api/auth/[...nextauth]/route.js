12fff47bbaf1431eed892c5e0858e83e3dea2366
dev-afk69 <fahad.64.in@gmail.com>
2026-01-02 10:58:06 +0530

    revert to nextauth format

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,23 +1,3 @@
-// Prevent Next.js from attempting to pre-render this route
-export const dynamic = 'force-dynamic'
+import { GET, POST } from "@/lib/auth"
 
-let handler = null
-
-async function initHandler() {
-  if (!handler) {
-    const NextAuth = (await import("next-auth")).default
-    const { authOptions } = await import("@/lib/auth")
-    handler = NextAuth(authOptions)
-  }
-  return handler
-}
-
-export async function GET(req, res) {
-  const h = await initHandler()
-  return h(req, res)
-}
-
-export async function POST(req, res) {
-  const h = await initHandler()
-  return h(req, res)
-}
+export { GET, POST }
